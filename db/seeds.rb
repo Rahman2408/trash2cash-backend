454c8df7e9976ac30513eea9ccb4bf1451f741f5
Rahman Elsirafy <rahman2408@gmail.com>
2021-10-04 14:22:46 -0400

    Adds some more seed data to test different scenaraios and/or missing attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,5 @@
 Item.create(name: "Soccer Ball", description: "Barely used official size soccer ball, mint condition.", price: 10, for_sale: true , image_url: "https://d34xpxcpbnz20f.cloudfront.net/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/b/a/barcelona_prestige_soccer_ball_equipment_.jpeg")
 Item.create(name: "HP Laptop", description: "2010 HP Laptop with Windows 7 installed", price: 100, for_sale: true, image_url: "https://ssl-product-images.www8-hp.com/digmedialib/prodimg/lowres/c06599034.png" )
 Item.create(name: "Coffee Maker", description: "Single-serve coffee machine. Doesn't use keurig cups.", price: 15, for_sale: true, image_url: "https://m.media-amazon.com/images/I/51jpge0lUsL._AC_SL1200_.jpg")
+Item.create(name:'macbook', description:'not selling this yet but maybe', price: 400, for_sale: false, image_url: "https://png.pngitem.com/pimgs/s/133-1333747_transparent-apple-computer-png-apple-macbook-pro-2009.png")
+Item.create(name:'Test Item', description:'test item with no image and forSale attribute also false', price: 400, for_sale: false)
