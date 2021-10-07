8000bc0eb1a817a55600903fae5fe68543b21296
Rahman Elsirafy <rahman2408@gmail.com>
2021-10-07 07:05:34 -0400

    Item controller now has create action and item_params to persist an Item object (acquired from the frontend) to the database as well as render it in JSON format

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,15 @@ class ItemsController < ApplicationController
     items = Item.all 
     render json: items 
   end
+
+  def create 
+    item = Item.create(item_params)
+    render json: item 
+  end
+
+  private 
+
+  def item_params
+    params.require(:item).permit(:name, :price, :image_url, :description, :for_sale)
+  end
 end
